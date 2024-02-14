e0bf30e65d1ebed50b17f9933500bb263ee7453d
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-02-14 23:37:48 +0200

    Add files via upload

---
+++ reviewslider.js
@@ -0,0 +1,36 @@
+var splide = new Splide("#main-slider", {
+    width: 1300,
+    height: 270,
+    pagination: false,
+    cover: true
+    });
+
+  var thumbnails = document.getElementsByClassName("thumbnail");
+  var current;
+
+  for (var i = 0; i < thumbnails.length; i++) {
+    initThumbnail(thumbnails[i], i);
+  }
+
+  function initThumbnail(thumbnail, index) {
+    thumbnail.addEventListener("click", function () {
+      splide.go(index);
+    });
+  }
+
+  splide.on("mounted move", function () {
+    var thumbnail = thumbnails[splide.index];
+
+    if (thumbnail) {
+      if (current) {
+        current.classList.remove("is-active");
+      }
+
+      thumbnail.classList.add("is-active");
+      current = thumbnail;
+    }
+  });
+
+
+
+  splide.mount();
